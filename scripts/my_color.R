ca0e18023ac9ef28c534dbecbc38524d22a58576
bakeronit <bakerzj36@gmail.com>
2022-06-06 11:12:00 +1000

    update maps,PCA,admixture,relatedness, etc.

--- scripts/my_color.R
+++ scripts/my_color.R
@@ -7,12 +7,15 @@ site_labels <- function(){
 }
 
 site_order <- function(){
-  c('MI'=0,'PI'=3,'DI'=5,'PR'=1,'FI'=7,"ARL"=2,"TAY"=4,"RIB"=6,"JB"=8)
+  c('MI'=0,'PI'=5,'DI'=3,'PR'=7,'FI'=1,"ARL"=2,"TAY"=4,"RIB"=6,"JB"=8)
+  c('MI'=0,'PI'=3,'DI'=5,'PR'=1,'FI'=7,"ARL"=8,"TAY"=6,"RIB"=4,"JB"=2)
 }
 
 site_colors <- function(){
   clrs <- c("#B15928","#FB9A99","#FDBF6F","#A6CEE3","#B2DF8A","#E31A1C","#FF7F00","#1F78B4","#33A02C")
-  names(clrs) <- c("MI","PR","DI","PI","FI","ARL","RIB","TAY","JB")
+  clrs <- c("#B15928","#FB9A99","#FDBF6F","#A6CEE3","#B2DF8A","#E31A1C","#FF7F00","#1F78B4","#33A02C")
+  names(clrs) <- c("MI","FI","PI","DI","PR","ARL","RIB","TAY","JB")
   clrs
 }
+
 
